b0ec148eb7f7bb0cd77f51c4600480a26b072ddd
beastraiser <alex.bulc.96@gmail.com>
2024-05-06 16:39:18 +0200

    v0.6

--- Icp.HotelAPI/Controllers/TipoCamasController/TipoCamasController.cs
+++ Icp.HotelAPI/Controllers/TipoCamasController/TipoCamasController.cs
@@ -1,11 +1,14 @@
 ﻿using AutoMapper;
 using Icp.HotelAPI.BBDD.FCT_ABR_11Context;
+using Icp.HotelAPI.Controllers.CategoriasController.DTO;
+using Icp.HotelAPI.Controllers.TipoCamasController.DTO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Icp.HotelAPI.Controllers.TipoCamasController
 {
     [ApiController]
-    [Route("api/habitaciones")]
+    [Route("api/tipocama")]
     public class TipoCamasController : ControllerBase
     {
         private readonly FCT_ABR_11Context context;
@@ -16,5 +19,16 @@ namespace Icp.HotelAPI.Controllers.TipoCamasController
             this.context = context;
             this.mapper = mapper;
         }
+
+        // Obtener todos los tipos de cama
+        [HttpGet]
+        public async Task<ActionResult<List<TipoCamaDTO>>> Get()
+        {
+            var entidades = await context.TipoCamas.ToListAsync();
+            var dtos = mapper.Map<List<TipoCamaDTO>>(entidades);
+            return dtos;
+        }
     }
+
+    
 }
